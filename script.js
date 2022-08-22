cf3d5727d5232468e86ee080711b8a41cc8faab3
prakash888 <frank_prakash8@yahoo.com>
2022-08-21 20:34:52 -0500

    javascript

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+const myCountry = "USA"
+console.log(myCountry);
+
+document.write('Chelsea')
+
+const age = 25;
+console.log(age);
+
+//Primitive- Datatypes like Number and String
+//Non Primitive- Array, Object, Function-Object
+
+const evenNumbers = [1,2,3,4,5,6,7, "Hello",[1,2,3]];
+ console.log( evenNumbers[8]);
+
+ //object- it does not have to be in any order
+
+ const bootcamp ={
+    course: "React",
+    duration: "3 months"
+ };
+ console.log(bootcamp.duration);
+
+ function add(first, second){
+    return first + second;
+
+ }
+
+ const result = add(10,20);
+ console.log(result);
+
+ const secondResult = add(2323, 2324)
+ console.log(secondResult);
